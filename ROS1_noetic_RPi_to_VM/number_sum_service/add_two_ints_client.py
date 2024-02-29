2cca5066f6d1c3b49a35352c72eeec88330416fd
SROP6313 <103128273+SROP6313@users.noreply.github.com>
2024-02-29 20:59:56 +0800

    Create add_two_ints_client.py

---
+++ ROS1_noetic_RPi_to_VM/number_sum_service/add_two_ints_client.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import sys
+import rospy
+from service_example.srv import AddTwoInts
+
+def add_two_ints_client(x, y):
+    rospy.wait_for_service('add_two_ints')
+    try:
+        add_two_ints = rospy.ServiceProxy('add_two_ints', AddTwoInts)
+        resp1 = add_two_ints(x, y)
+        return resp1.sum
+    except rospy.ServiceException as e:
+        print("Service call failed: %s"%e)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        x = int(sys.argv[1])
+        y = int(sys.argv[2])
+    else:
+        sys.exit(1)
+    print("Requesting %s+%s"%(x, y))
+    print("%s + %s = %s"%(x, y, add_two_ints_client(x, y)))
